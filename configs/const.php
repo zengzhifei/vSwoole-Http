f97167b0df2cd87767e1cf90d8625a6b57f07ef7
zengzhifei <zengzhifei@ctvit.com.cn>
2018-11-06 17:20:08 +0800

    vSwoole-Http

---
+++ configs/const.php
@@ -0,0 +1,16 @@
+<?php
+// +----------------------------------------------------------------------+
+// | VSwoole FrameWork                                                    |
+// +----------------------------------------------------------------------+
+// | Not Decline To Shoulder a Responsibility                             |
+// +----------------------------------------------------------------------+
+// | zengzhifei@outlook.com                                               |                  
+// +----------------------------------------------------------------------+
+
+/**
+ * 服务+别名注册
+ * defined('VSWOOLE_SERVERNAME_SERVER') or define('VSWOOLE_SERVERNAME_SERVER', 'Swoole_Servername_Server') and define('Servername', VSWOOLE_SERVERNAME_SERVER);
+ */
+
+//Http服务
+defined('VSWOOLE_HTTP_SERVER') or define('VSWOOLE_HTTP_SERVER', 'Swoole_Http_Server') and define('Http', VSWOOLE_HTTP_SERVER);
